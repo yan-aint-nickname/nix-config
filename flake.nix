9745e8acfa2d81e6bb8be4d6d6a5c13703bc6bc7
kirsh-ya <kirsh-ya@ZVQ-H75P9N6Y72.local>
2023-03-08 19:57:45 +0300

    Initial config

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "my mininal flake";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+    home-manager.url = "github:nix-community/home-manager/master";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    darwin.url = "github:lnl7/nix-darwin";
+    darwin.inputs.nixpkgs.follows = "nixpkgs";
+  };
+  outputs = inputs@{nixpkgs, home-manager, darwin, ...}: {
+    darwinConfigurations.ZVQ-H75P9N6Y72 = darwin.lib.darwinSystem {
+      system = "aarch64-darwin";
+      pkgs = import nixpkgs { system = "aarch64-darwin"; };
+      modules = [
+        ./modules/darwin
+        home-manager.darwinModules.home-manager
+        {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            users.kirsh-ya.imports = [ ./modules/home-manager ];
+          };
+        }
+      ];
+    };
+  };
+}
---
+++ modules/home-manager/default.nix
@@ -0,0 +1,30 @@
+{ pkgs, ... }: {
+  # Don't change this when you change package input. Leave it alone.
+  home.stateVersion = "22.11";
+  # specify my home-manager configs
+  home.packages = with pkgs; [
+    fzf
+    curl
+    less
+    neovim
+  ];
+  home.sessionVariables = {
+    PAGER = "less";
+    CLICLOLOR = 1;
+    EDITOR = "nvim";
+  };
+  programs.fzf.enable = true;
+  programs.fzf.enableZshIntegration = true;
+  programs.git.enable = true;
+  programs.zsh.enable = true;
+  programs.zsh.enableCompletion = true;
+  programs.zsh.enableAutosuggestions = true;
+  programs.zsh.enableSyntaxHighlighting = true;
+  programs.zsh.shellAliases = {
+    ls = "ls --color=auto -F";
+    nixswitch = "darwin-rebuild switch --flake ~/.nix-config/.#";
+    nixup = "pushd ~/.nix-config; nix flake update; nixswitch; popd";
+  };
+  programs.starship.enable = true;
+  programs.starship.enableZshIntegration = true;
+}
